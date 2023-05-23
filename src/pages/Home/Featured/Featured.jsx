429ecd8ad9d2447f43901d2fcf587e2b704f1d09
SQLAdmin <abuyeahia24@gmail.com>
2023-05-23 15:12:28 +0600

    added featured item parallax

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,14 +4,14 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle
                 subHeading="check it Out"
                 heading="Featured Item"
             >
 
             </SectionTitle>
-            <div className="md:flex justify-center items-center py-20 px-36 pt-12">
+            <div className="md:flex justify-center bg-slate-500 bg-opacity-60 items-center py-20 px-36 pt-12">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -19,7 +19,7 @@ const Featured = () => {
                     <p>Aug 20, 2023</p>
                     <p>Where can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
 
