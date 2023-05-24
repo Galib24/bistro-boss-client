f24f45efba18cfb91304d6e00104fede2c8eea56
SQLAdmin <abuyeahia24@gmail.com>
2023-05-24 18:13:52 +0600

    todays offer added

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,19 +1,22 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../../Components/Section Title/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../Hooks/useMenu";
 
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([])
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'popular');
-                setMenu(popularItems)
-            })
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular');
+    // const [menu, setMenu] = useState([])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const popularItems = data.filter(item => item.category === 'popular');
+    //             setMenu(popularItems)
+    //         })
 
-    }, [])
+    // }, [])
     return (
         <section className="mb-12">
             <SectionTitle
@@ -26,7 +29,7 @@ const PopularMenu = () => {
             </SectionTitle>
             <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item => <MenuItem
+                    popular.map(item => <MenuItem
 
                         key={item._id}
                         item={item}
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,9 +1,18 @@
+/* eslint-disable no-unused-vars */
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import useMenu from '../../../Hooks/useMenu';
+import SectionTitle from '../../../Components/Section Title/SectionTitle';
+import MenuCategory from '../Menu Category/MenuCategory';
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const dessert =  menu.filter(item => item.category === 'dessert');
+    const soup =  menu.filter(item => item.category === 'soup');
+    const salad =  menu.filter(item => item.category === 'salad');
+    const pizza =  menu.filter(item => item.category === 'pizza');
+    const offered =  menu.filter(item => item.category === 'offered');
     return (
         <div>
             <Helmet>
@@ -11,11 +20,14 @@ const Menu = () => {
                
             </Helmet>
             <Cover img={menuImg} title='Our Menu'></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title='Our Menu'></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title='Our Menu'></Cover>
-            <PopularMenu></PopularMenu>
+            <SectionTitle
+            subHeading={"Don't Miss"}
+            heading={"Today's Offer"}
+            >
+
+            </SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
+          
         </div>
     );
 };
