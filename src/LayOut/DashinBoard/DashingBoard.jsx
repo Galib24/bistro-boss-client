57c97efeceaebcb3afd4db3663f8771649431923
SQLAdmin <abuyeahia24@gmail.com>
2023-05-28 18:48:33 +0600

    added my cart section cart item and total price

--- src/LayOut/DashinBoard/DashingBoard.jsx
+++ src/LayOut/DashinBoard/DashingBoard.jsx
@@ -1,22 +1,29 @@
-import { Link, Outlet } from "react-router-dom";
-import { FaShoppingCart, } from 'react-icons/fa';
+import { NavLink, Outlet } from "react-router-dom";
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from 'react-icons/fa';
 
 
 const DashingBoard = () => {
     return (
-        <div className="drawer drawer-mobile">
+        <div className="drawer drawer-mobile ">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 <Outlet></Outlet>
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
             </div>
-            <div className="drawer-side">
+            <div className="drawer-side ">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-                  
-                    <li><Link><FaShoppingCart></FaShoppingCart>My Cart</Link> </li>
-                    <li><a>Sidebar Item 2</a></li>
+                <ul className="menu p-4 w-80 bg-[#D1A054] text-base-content">
+
+                    <li><NavLink to='/dashboard/home'><FaHome></FaHome>User Home</NavLink></li>
+                    <li><NavLink to='/dashboard/reservation'><FaCalendarAlt></FaCalendarAlt> Reservations </NavLink></li>
+                    <li><NavLink to='/dashboard/history'><FaWallet></FaWallet>Payment History</NavLink></li>
+                    <li><NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart</NavLink> </li>
+                    <div className="divider"></div>
+                    <li><NavLink to='/'><FaHome></FaHome>Home</NavLink></li>
+                    <li><NavLink to="/menu">Our Menu</NavLink></li>
+                    <li><NavLink to="/order/salad">Order Food</NavLink></li>
+
                 </ul>
 
             </div>
