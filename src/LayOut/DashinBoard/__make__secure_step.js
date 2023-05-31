e0e332e80185a86bf78ee1d83574bf48eda75ecd
SQLAdmin <abuyeahia24@gmail.com>
2023-06-01 04:09:46 +0600

    added image hosting to add item

--- src/LayOut/DashinBoard/__make__secure_step.js
+++ src/LayOut/DashinBoard/__make__secure_step.js
@@ -4,7 +4,14 @@
  * 
  * 3) do not allow to visit the link by typing on the url.
  * 
+ * -----------------------------------
+ *  to send data
  * 
+ * ----------
+ * 
+ * 1) verify jwt token
+ * 2) if it is an admin activity make sure only admin user is posting data
+ * by using verifyAdmin
  * 
  * 
  * 
---
+++ src/pages/Dashboard Items/temp form hook/temp.jsx
@@ -0,0 +1,25 @@
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input 
+        {...register("firstName", { required: true })} 
+        aria-invalid={errors.firstName ? "true" : "false"} 
+      />
+      {errors.firstName?.type === 'required' && <p role="alert">First name is required</p>}
+
+      <input 
+        {...register("mail", { required: "Email Address is required" })} 
+        aria-invalid={errors.mail ? "true" : "false"} 
+      />
+      {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+      
+      <input type="submit" />
+    </form>
+  );
+}
